807ee0aa14dbc412e3b46f12dccf303b3f39b0fa
AmbatiVikithaReddy <ambativikithareddy726@gmail.com>
2026-02-14 19:07:22 +0530

    Add files via upload

---
+++ vowelchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    char name[100];
+
+    printf("Enter the name:\n");
+    scanf("%s", name);
+
+    int size = strlen(name);
+
+    for (int i = 0; i <= size - 1; i++)
+    {
+        char ch = name[i];
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+        {
+            printf("%c\n", ch);
+        }
+    }
+
+    return 0;
+}
